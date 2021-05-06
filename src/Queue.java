379493d4c0c3e5ad10366df7cd1c2349cebbea22
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-05-06 16:50:33 +0530

    First Queue commit

---
+++ src/Queue.java
@@ -0,0 +1,36 @@
+public class Queue {
+
+    public void enqueue(int number){
+
+    }
+
+    public void dequeue(){
+
+    }
+
+    public boolean empty(){
+        return true;
+    }
+
+    public int peek(){
+       return 0;
+    }
+
+    public void clear(){
+
+    }
+
+    public void print(){
+
+    }
+
+    public boolean contains(int number){
+        return true;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+
+}
---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+
+
+    }
+}
