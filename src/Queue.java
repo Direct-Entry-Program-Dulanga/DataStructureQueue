9675ebe0b5089d88918269a01f1a4b98bed5f231
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-05-06 17:08:41 +0530

    enqueue and print commit

--- src/Queue.java
+++ src/Queue.java
@@ -1,7 +1,19 @@
 public class Queue {
 
-    public void enqueue(int number){
+    private int[] ArrayQ;
 
+    public void enqueue(int number){
+        if(ArrayQ == null){
+            ArrayQ = new int[1];
+            ArrayQ[0] = number;
+        }else{
+            int[] temp = new int[ArrayQ.length+1];
+            for (int i = 0; i < ArrayQ.length; i++) {
+                temp[i] = ArrayQ[i];
+            }
+            temp[temp.length-1] = number;
+            ArrayQ = temp;
+        }
     }
 
     public void dequeue(){
@@ -9,11 +21,14 @@ public class Queue {
     }
 
     public boolean empty(){
-        return true;
+        return ArrayQ == null;
     }
 
     public int peek(){
-       return 0;
+        if (empty()){
+            System.err.println("Stack is empty");
+        }
+        return ArrayQ[ArrayQ.length];
     }
 
     public void clear(){
@@ -21,7 +36,16 @@ public class Queue {
     }
 
     public void print(){
-
+        if(empty()){
+            System.out.println("[]");
+            return;
+        }
+        System.out.print("[ ");
+        for (int i = 0; i < ArrayQ.length ; i++) {
+            System.out.print(ArrayQ[i]+" ");
+        }
+        System.out.print(" \b]");
+        System.out.println("");
     }
 
     public boolean contains(int number){
--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,15 @@ public class Test {
     public static void main(String[] args) {
         Queue queue = new Queue();
 
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.enqueue(60);
+        queue.enqueue(70);
+        queue.print();
+
 
     }
 }
